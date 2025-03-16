885c879e0b0013068aacc1ca5593ee3802603c83
AARONJOYK <aaronjoyk@gmail.com>
2025-03-16 19:45:36 +0000

    Added extra vulnerabilities for security analysis

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+
+#new code
+
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
